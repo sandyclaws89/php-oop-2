afebca4bc74820fb9020b643cbe72877e801c8b7
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-05-02 16:55:17 +0200

    impostazione classi

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         private $maxVelocity;
         private $weight;
         private $nPassengers;
-        private $builder;
+        protected $builder;
         private $destinationUse;
 
         public function getBuilder()
@@ -27,25 +27,65 @@
         {
             $this->weight = $newWeight;
         }
+        public function setBuilder($builder)
+        {
+            $this->builder = $builder;
+            return $this;
+        }
 
     }
 
     $obj = new Authovehicle();
     // $obj->nTires = 150;
-    $obj->modifyWeight(200);
+    // $obj->modifyWeight(200);
 
     
     // var_dump($weight);
     $obj->setBuilder('fiat');
     var_dump($obj);
 
+?>
 
+<?php
+    class product
+    {
+        private $id;
+        private $price;
+        private $type;
+        private $animalType;
+        private $availability;
+    } 
+    class food extends product
+    {
+        private $expirationDate;
+        private $packingDate;
+        private $ingredients;
+    }
+    class accessories extends product
+    {
+        private $material;
+    }
 
+    class user 
+    {
+        private $idVisiter;
+        private $visitDate;
+        
+    }
+    class registeredUser extends user
+    {
+        private $idAccount;
+        private $accountName;
+        private $registrationDate;
+        private $email;
+    }
+    class buyerUser extends user
+    {
+        private $transaction;
+    }
+    class buyerRegisteredUser extends user
+    {
+        private $discountedTransaction;
+    }
 
-
-
-
-
-    
-    ?>
   
